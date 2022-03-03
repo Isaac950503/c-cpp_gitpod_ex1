a5a1b7c44480d4769f7cc66740feb589de554cf0
Isaac950503 <100347978+Isaac950503@users.noreply.github.com>
2022-03-03 19:17:39 +0800

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,15 @@
+int main()
+{   
+    int myage = 16;
+    int mybirthday =950503;
+    int myheight=171;
+    int myweight=75;
+printf("I am %d years old \n",myage );
+printf("My birthday is%d  \n",mybirthday );
+printf("My height is%d cm \n" ,myheight);
+printf("My weight is%d kg \n" ,myweight);
+//printf("" );
+    cout<<"Hello World";
+
+    return 0;
+}
