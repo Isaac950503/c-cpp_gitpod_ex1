006e99948ebcb3301affb42b95231a54f583a530
Isaac950503 <11011194@student.kjsh.ntpc.edu.tw>
2022-03-17 10:45:58 +0000

    udpate

---
+++ ex6.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, product;
+    a = 20;
+    b = 7;
+
+ /*
+x + y
+x - y
+x * y
+x / y
+x % y
+++x
+x++
+--x
+x--
+
+x = 16 
+x >> 1
+x >> 2
+x << 1
+ */
+    // Calculating product
+    product = a % b;
+
+ 
+    printf("a mod b = %d \n", product);
+
+    product = a + b;
+   
+    printf("a + b = %d \n", product);
+
+    product = a - b;
+
+ 
+    printf("a - b= %d \n", product);
+     
+     product = a * b;
+    
+    printf("a * b = %d \n", product);
+
+     product = a / b;
+
+ 
+    printf("a / b= %d \n", product);
+    return 0;
+}
