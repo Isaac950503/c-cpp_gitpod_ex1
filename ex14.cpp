9652ca005cffeb188134aeb19571f6d8946f47e6
Isaac950503 <11011194@student.kjsh.ntpc.edu.tw>
2022-04-07 11:48:03 +0000

    update

---
+++ ex14.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int j,i;
+    
+        for(i=1;i<=6;i++){
+        for(j=1;j<=6-i;j++){
+        //cout << j <<"* " << i << "= " <<  j*i  << "  ";
+        printf(" %d ",i);
+        }
+        cout << endl;
+    }
+    
+    
+    cout<<"Hello World";
+
+    return 0;
+}
