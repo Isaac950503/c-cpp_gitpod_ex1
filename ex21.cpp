0124e2830592083dd24230ff3fe69212b8840f5a
Isaac950503 <11011194@student.kjsh.ntpc.edu.tw>
2022-05-05 10:44:01 +0000

    update

---
+++ ex21.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+#define n 10 
+int main()
+{
+   int i = 1,j = 1;
+   while(i < n ){
+       
+   while(j < n ){
+       printf(" %d*%d = %d \n",i,j,i*j);
+       j++;
+       
+       
+   }i++;j=1;}
+
+
+   
+   return 0;
+}
+
+
+
