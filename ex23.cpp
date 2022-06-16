3a9edf0af107442fd0a9fcfac10d304d2fc89cba
Isaac950503 <11011194@student.kjsh.ntpc.edu.tw>
2022-06-16 10:52:37 +0000

    update

--- ex23.cpp
+++ ex23.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <stdio.h>
+//輸入一個圓半徑（r）當r>＝0時，計算並輸出圓的面積和周長，否則，輸出提示資訊。
 using namespace std;
 
 int main()
 {
   float r,b;
   scanf("%f",&r);
-  
-  printf("r*r*3.14 = %f",r*r*3.14);
+  if(r>=0){    
+     
+     printf("r*r*3.14 = %f",r*r*3.14);
+      
+  }else{
+      printf("r>=0");    
+  }
 }
