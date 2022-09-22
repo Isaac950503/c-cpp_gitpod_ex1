2e7143ce4df5eaa792ec8107fdcef9a92562b919
Isaac950503 <100347978+Isaac950503@users.noreply.github.com>
2022-09-22 19:23:56 +0800

    Create ex28.cpp

---
+++ ex28.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, num;
+    printf("please input :");
+    scanf("%d",&num);
+    
+    printf(" number + 10 = %d \n", num + 10);
+    for(i=0;i< 10 ;i++){
+        if( i % 2 == 0 ){
+        printf("i = %d , ",i);
+        }
+    }
+    #if 0
+    for(){
+        if(){
+            
+        }
+    }
+    #endif
+    return 0;
+}
