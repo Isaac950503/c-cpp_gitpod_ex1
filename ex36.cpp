1503fc71b56836e324c6f6f5684a9db95656f876
Isaac950503 <100347978+Isaac950503@users.noreply.github.com>
2022-11-10 19:32:07 +0800

    Create ex36.cpp

---
+++ ex36.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,sum=0;
+    for(i=1;i<1000;i=i+2){
+        sum = sum + i;
+        printf("%d\n",sum);
+        
+    }
+    
+    cout<<"Hello World";
+
+    return 0;
+}
