b4305c5713e62f12b6a89e91b04569dae80bb6c6
Isaac950503 <100347978+Isaac950503@users.noreply.github.com>
2022-11-17 19:31:57 +0800

    Create ex37.cpp

---
+++ ex37.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//4．有一函式當 x < 0 時 y=1，當 x>0 時，y=3，當 x=0 時 y=5，程式設計，從鍵盤輸入一個x值，輸出y值。
+using namespace std;
+int fun(int x){
+    if( x < 0 ){
+        
+    }else if (){
+        
+    }else{
+        
+    }
+    
+    return y;
+}
+int main(){
+    int a,b;
+    
+    printf("please input two numbers");
+    scanf("%d",&a);
+    //scanf("%d",&b);
+    y = fun
+    printf (" y = %d \n", b)
+    return 0;
+}
