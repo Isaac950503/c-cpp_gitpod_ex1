c22c4d59a624f8042c176611555044de6d32fa6a
Isaac950503 <100347978+Isaac950503@users.noreply.github.com>
2022-11-24 18:47:47 +0800

    Create ex40.cpp

---
+++ ex40.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10];
+    int n,i;
+    int sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+        sum += a[i];
+        // sum = sum + a[i]
+    }
+    printf("\n sum = %d \n",sum);
+    
+    
+
+    return 0;
+}
