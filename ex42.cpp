251171c40d651cd366278d5b723e3af2d770399b
Isaac950503 <100347978+Isaac950503@users.noreply.github.com>
2022-12-08 19:30:09 +0800

    Create ex42.cpp

---
+++ ex42.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10];
+    int n,i;
+    int sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+        sum += a[i];
+        // sum = sum + a[i]
+    }
+    printf("\n");
+    //scanf("");
+    printf("sum = %d \n",sum);
+    printf("average = %d \n",sum/n);
+
+    return 0;
+}
